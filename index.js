99f8e84ba172d82e28a7c31318bea2e4cafd8718
Alexandre-Chs <oliuuum@gmail.com>
2023-10-27 14:10:48 +0200

    add : - setup new project with my token

---
+++ index.js
@@ -0,0 +1,10 @@
+import { Client, GatewayIntentBits, Events } from "discord.js";
+import "dotenv/config";
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
